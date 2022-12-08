837900cf3ddf29ceac66f493c87202d5327804cf
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-12-08 21:06:10 +0300

    init project

---
+++ tests/Unit/SolveTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SolveTest extends TestCase
+{
+    public function testHaveNotRoots()
+    {
+        $this->assertSame(true, true);
+    }
+}
