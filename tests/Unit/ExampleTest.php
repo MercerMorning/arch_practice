0b0bc868180eca39a3a560b4e52efd8421f45418
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-12-15 18:23:00 +0300

    added example test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertSame(true, true);
+    }
+}
