47690825812055933dceb35adc14ce4ec90dbfd3
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2022-12-18 12:43:24 +0300

    Удалена лишняя проверка в CheckLessThanZero

--- src/AppBundle/Domains/EquationManager.php
+++ src/AppBundle/Domains/EquationManager.php
@@ -46,7 +46,7 @@ class EquationManager
      */
     private static function CheckLessThanZero(float $num): bool
     {
-        return abs($num) < PHP_FLOAT_EPSILON && abs($num) >= 0;
+        return abs($num) < PHP_FLOAT_EPSILON;
     }
 
     /**
