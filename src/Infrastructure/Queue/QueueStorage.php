bb21dbaa08c39ed657b7ea7a9ba28d13990929fc
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2023-01-18 17:15:49 +0300

    Добавлена реализация для QueueStorage.php

--- src/Infrastructure/Queue/QueueStorage.php
+++ src/Infrastructure/Queue/QueueStorage.php
@@ -10,7 +10,6 @@ class QueueStorage implements QueueStorageInterface
 
     public function take(): ?CommandInterface
     {
-        var_dump(count(self::$queue));
         return array_pop(self::$queue);
     }
 
