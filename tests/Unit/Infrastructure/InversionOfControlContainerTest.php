b90c62477b9ae14c708df8900317a0532c743860
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2023-01-22 14:25:48 +0300

    Автогенерация классов в процессе

--- tests/Unit/Infrastructure/InversionOfControlContainerTest.php
+++ tests/Unit/Infrastructure/InversionOfControlContainerTest.php
@@ -3,18 +3,25 @@
 namespace Tests\Unit\Infrastructure;
 
 
-use App\Infrastructure\InversionOfControlContainer;
+use App\Domain\TankOperationsMovableInterface;
+use App\Infrastructure\IoC;
 use PHPUnit\Framework\TestCase;
 
 class InversionOfControlContainerTest  extends TestCase
 {
     public function testResolve()
     {
-       $container = new InversionOfControlContainer();
+       $container = new IoC();
        $container->resolve('IoC.Register', 'A', function (array $args){
             return $args[0];
        });
        $result = $container->resolve('A', 'test');
        $this->assertSame('test', $result);
+    }
+
+    public function testAutoGenerating()
+    {
+        $container = new IoC();
+        $container->generate(TankOperationsMovableInterface::class);
     }
 }
