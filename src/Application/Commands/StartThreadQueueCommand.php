53eea7b9f6873e38b5525f1b5b54c8f5e45ff243
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2023-02-04 15:07:31 +0300

    Реализованы команды

--- src/Application/Commands/StartThreadQueueCommand.php
+++ src/Application/Commands/StartThreadQueueCommand.php
@@ -2,14 +2,30 @@
 
 namespace App\Application\Commands;
 
+use App\Infrastructure\Exceptions\ExceptionHandlerInterface;
 use App\Infrastructure\InversionOfControlContainer;
-use App\Infrastructure\Queue\QueueListener;
+use App\Infrastructure\Queue\QueueStorageInterface;
+use Exception;
 
 class StartThreadQueueCommand implements CommandInterface
 {
-    public function execute()
-    {
-        $queueListener = InversionOfControlContainer::resolve(QueueListener::class);
+    private QueueStorageInterface $queueStorage;
+    public ExceptionHandlerInterface $errorHandler;
 
+    public function __construct()
+    {
+        $this->queueStorage = InversionOfControlContainer::getInstance()->resolve(QueueStorageInterface::class);
+        $this->errorHandler = InversionOfControlContainer::getInstance()->resolve(ExceptionHandlerInterface::class);
+    }
+
+    public function execute(): void
+    {
+        while ($queueItem = $this->queueStorage->take()) {
+            try {
+                $queueItem->execute();
+            } catch (Exception $exception) {
+
+            }
+        }
     }
 }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Application\Commands\CommandInterface;
+use App\Application\Commands\ForceQueueStopCommand;
 use App\Application\Commands\SoftQueueStopCommand;
 use App\Infrastructure\Exceptions\CommandExceptionHandler;
 use App\Infrastructure\Exceptions\ExceptionHandlerInterface;
@@ -11,12 +12,11 @@ use App\Infrastructure\InversionOfControlContainer;
 use App\Infrastructure\Queue\QueueListener;
 use App\Infrastructure\Queue\QueueStorage;
 use App\Infrastructure\Queue\QueueStorageInterface;
-use App\Infrastructure\ScopeBasedResolveDependencyStrategy;
 use PHPUnit\Framework\TestCase;
 
 class ThreadTest extends TestCase
 {
-    public function testStartThread()
+    public function testSoftQueueStop()
     {
         $container = new InversionOfControlContainer();
         InversionOfControlContainer::setInstance($container);
@@ -27,7 +27,7 @@ class ThreadTest extends TestCase
         $container->resolve("IoC.Register", QueueStorageInterface::class, function () {
             return new QueueStorage();
         })->execute();
-        $container->resolve("IoC.Register",ExceptionHandlerInterface::class, function () {
+        $container->resolve("IoC.Register", ExceptionHandlerInterface::class, function () {
             return new CommandExceptionHandler();
         })->execute();
 
@@ -39,6 +39,10 @@ class ThreadTest extends TestCase
             );
         })->execute();
 
+        $container->resolve("IoC.Register", 'stopThread', function () {
+            return new SoftQueueStopCommand();
+        })->execute();
+
         /**
          * @var $listener QueueListener
          */
@@ -48,7 +52,55 @@ class ThreadTest extends TestCase
             [
                 $this->createMock(CommandInterface::class)->method('execute')->willReturn('1'),
                 $this->createMock(CommandInterface::class)->method('execute')->willReturn('2'),
-                new SoftQueueStopCommand(),
+                $container->resolve("stopThread"),
+                $this->createMock(CommandInterface::class)->method('execute')->willReturn('3'),
+            ];
+
+        foreach ($commands as $command) {
+            QueueStorage::push($command);
+        }
+
+
+        $listener->listen();
+    }
+
+    public function testForceQueueStop()
+    {
+        $container = new InversionOfControlContainer();
+        InversionOfControlContainer::setInstance($container);
+        $command = new InitScopeBasedIoCImplementation();
+        $command->execute();
+
+
+        $container->resolve("IoC.Register", QueueStorageInterface::class, function () {
+            return new QueueStorage();
+        })->execute();
+        $container->resolve("IoC.Register", ExceptionHandlerInterface::class, function () {
+            return new CommandExceptionHandler();
+        })->execute();
+
+
+        $container->resolve("IoC.Register", QueueListener::class, function () {
+            return new QueueListener(
+                InversionOfControlContainer::getInstance()->resolve(QueueStorageInterface::class),
+                InversionOfControlContainer::getInstance()->resolve(ExceptionHandlerInterface::class),
+            );
+        })->execute();
+
+        $container->resolve("IoC.Register", 'stopThread', function () {
+            return new ForceQueueStopCommand();
+        })->execute();
+
+        /**
+         * @var $listener QueueListener
+         */
+        $listener = $container->resolve(QueueListener::class);
+
+        $commands =
+            [
+                $this->createMock(CommandInterface::class)->method('execute')->willReturn('1'),
+                $this->createMock(CommandInterface::class)->method('execute')->willReturn('2'),
+                $container->resolve("stopThread"),
                 $this->createMock(CommandInterface::class)->method('execute')->willReturn('3'),
             ];
 
