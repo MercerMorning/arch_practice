a3729438d5a0b947c60991da34eddbdafd390523
ni.vasilev <mailto:ni.vasilev@andersenlab.com>
2023-02-08 13:34:01 +0300

    added route and controller

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once('../vendor/autoload.php');
+
+$router = new \App\Infrastructure\Route();
+$router::contentToRender();
---
+++ src/Infrastructure/Http/Controllers/SendCommandController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Infrastructure\Http\Controllers;
+
+class SendCommandController
+{
+    public static function index() {
+        var_dump(123);
+        exit();
+    }
+}
